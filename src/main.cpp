8a130a7851099567d0a396b79359e753b6bb84c1
Morgan Brown <morganb816@gmail.com>
2022-09-08 12:16:14 -0400

    feat: moving paddles

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,43 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <vector>
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <math.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+struct Position {
+    float x;
+    float y;
+    float z;
+};
+
+struct Renderable {
+    std::vector<float> vertices;
+    unsigned int VBO;
+    unsigned int VAO;
+};
+
+struct GameObject {
+    std::string id;
+    Renderable renderable;
+    glm::vec3 position;
+    glm::vec3 rotation;
+};
+
+struct Camera {
+    glm::mat4 view;
+    glm::vec3 position;
+    glm::mat4 projection;
+};
 
 /**
  * @brief Update OpenGL Viewport size through GLFW callback
@@ -45,7 +76,7 @@ GLFWwindow* initializeGLFWandGLAD() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Attempt to create GLFW window
-    GLFWwindow *window_p = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window_p = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "PONG | Morgan Brown", NULL, NULL);
     if (window_p == NULL) {
         std::cerr << "Failed to create glfw window" << std::endl;
         glfwTerminate();
@@ -61,7 +92,7 @@ GLFWwindow* initializeGLFWandGLAD() {
     }
 
     // Set OpenGL viewport size
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     // Assign a callback to update the OpenGL viewport size whenever the
     // GLFW window resizes
     glfwSetFramebufferSizeCallback(window_p, framebuffer_size_callback);
@@ -137,6 +168,32 @@ bool initializeShaderFromFile(std::string filePath, GLenum shaderType, unsigned 
     return true;
 }
 
+bool readAssetFile(std::string assetFilePath, std::vector<float> &verticies) {
+    std::string fileSource;
+    if (!readFileToString(assetFilePath, fileSource)) {
+        return false;
+    };
+    std::string currentFloat;
+    for (int i = 0; i < fileSource.size(); i ++) {
+        switch (fileSource[i]) {
+            case ',': 
+            case '\n': {
+                if (currentFloat == "") {
+                    continue;
+                }
+                verticies.push_back(atof(currentFloat.c_str()));
+                currentFloat = "";
+                break;
+            }
+            default: {
+                currentFloat.push_back(fileSource[i]);
+            }
+        }
+    }
+
+    return true;
+}
+
 /**
  * @brief Create a Shader Program object, and delete shaders provided.
  * 
@@ -145,10 +202,9 @@ bool initializeShaderFromFile(std::string filePath, GLenum shaderType, unsigned 
  * @return true 
  * @return false 
  */
-bool createShaderProgram(unsigned int shaders[], unsigned int &shaderProgram) {
+bool createShaderProgram(unsigned int shaders[], unsigned int shaderCount, unsigned int &shaderProgram) {
     shaderProgram = glCreateProgram();
 
-    int shaderCount = sizeof(shaders) / sizeof(unsigned int);
     for (int i = 0; i < shaderCount; i++) {
         glAttachShader(shaderProgram, shaders[i]);
         glDeleteShader(shaders[i]);     
@@ -167,20 +223,8 @@ bool createShaderProgram(unsigned int shaders[], unsigned int &shaderProgram) {
     return true;
 }
 
-struct Position {
-    float x;
-    float y;
-    float z;
-};
-
-struct Renderable {
-    std::vector<float> vertices;
-    unsigned int VBO;
-    unsigned int VAO;
-};
-
 Renderable createRenderable(float vertices[], unsigned int numberOfVerticies) {
-    float floatsPerVertexPoint = 3;
+    float floatsPerVertexPoint = 2;
     Renderable newRenderable;
 
     for (int i = 0; i < numberOfVerticies; i ++) {
@@ -205,6 +249,39 @@ Renderable createRenderable(float vertices[], unsigned int numberOfVerticies) {
     return newRenderable;
 }
 
+bool createGameObject(std::string id, std::string assetFilePath, GameObject &gameObject) {
+    std::vector<float> assetVerticies;
+    if (!readAssetFile(assetFilePath, assetVerticies)) {
+        return false;
+    }
+    
+    gameObject.renderable = createRenderable(&assetVerticies[0], assetVerticies.size());
+    gameObject.id = id;
+    gameObject.position = glm::vec3(0.0f);
+    gameObject.rotation = glm::vec3(0.0f);
+
+    return true;
+}
+
+Camera createCamera() {
+    Camera out;
+
+    out.view = glm::mat4(1.0f);
+    out.position = glm::vec3(0.0f, 0.0f, -1.0f);
+    out.view = glm::translate(out.view, out.position);
+
+    out.projection = glm::ortho(
+        0.0f,                       // Left
+        (float)WINDOW_WIDTH,        // Right
+        0.0f,                       // Bottom
+        (float)WINDOW_HEIGHT,       // Top
+        0.0f,                       // Near Clip
+        100.0f                      // Far Clip
+    );
+
+    return out;
+};
+
 void renderRenderable(Renderable renderable) {
     float verticesToRender[renderable.vertices.size()];
     std::copy(renderable.vertices.begin(), renderable.vertices.end(), verticesToRender);
@@ -212,9 +289,16 @@ void renderRenderable(Renderable renderable) {
     glBindVertexArray(renderable.VAO);
     glBindBuffer(GL_ARRAY_BUFFER, renderable.VBO);
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesToRender), verticesToRender, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesToRender), verticesToRender, GL_DYNAMIC_DRAW);
 
-    glDrawArrays(GL_TRIANGLES, 0, (sizeof(verticesToRender) / sizeof(float)) / 3); 
+    glDrawArrays(GL_TRIANGLES, 0, (sizeof(verticesToRender) / sizeof(float)) / 2); 
+}
+
+void renderGameObject(GameObject &gameObject, unsigned int modelUniformLocation) {
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, gameObject.position);
+    glUniformMatrix4fv(modelUniformLocation, 1, GL_FALSE, glm::value_ptr(model));
+    renderRenderable(gameObject.renderable);
 }
 
 void deleteRenderable(Renderable renderable) {
@@ -223,6 +307,7 @@ void deleteRenderable(Renderable renderable) {
 }
 
 int main() {
+
     GLFWwindow *window_p = initializeGLFWandGLAD();
     if (window_p == NULL) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
@@ -230,50 +315,69 @@ int main() {
 
     unsigned int vertexShader, fragmentShader;
     if (
-        !initializeShaderFromFile("src/shaders/vertexShader.GLSL", GL_VERTEX_SHADER, vertexShader) ||
-        !initializeShaderFromFile("src/shaders/fragmentShader.GLSL", GL_FRAGMENT_SHADER, fragmentShader)
+        !initializeShaderFromFile("shaders/vertexShader.GLSL", GL_VERTEX_SHADER, vertexShader) ||
+        !initializeShaderFromFile("shaders/fragmentShader.GLSL", GL_FRAGMENT_SHADER, fragmentShader)
     ) {
+        std::cerr << "Failed to load shaders" << std::endl;
         return -1;
     }
     
     // Create and use our shader program
     unsigned int shaderProgram;
     unsigned int shaders[2] = {vertexShader, fragmentShader};
-    if (!createShaderProgram(shaders, shaderProgram)) {
+    if (!createShaderProgram(shaders, 2, shaderProgram)) {
         return -1;
     }
+    glUseProgram(shaderProgram);
 
-    float paddleVerticies[] = {
-     0.5f, -0.5f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, // bottom left
-    -0.5f,  0.0f, 0.0f, // top left
-     0.5f, -0.5f, 0.0f, // bottom right
-    -0.5f,  0.0f, 0.0f, // top left
-     0.5f,  0.0f, 0.0f, // top right
-    }; 
-    Renderable paddle = createRenderable(paddleVerticies, 6 * 3);
-    float paddleVerticiesB[] = {
-     0.0f,  0.0f, 0.0f, // bottom right
-    -0.5f,  0.0f, 0.0f, // bottom left
-    -0.5f,  0.5f, 0.0f, // top left
-     0.5f,  0.0f, 0.0f, // bottom right
-    -0.5f,  0.5f, 0.0f, // top left
-     0.5f,  0.5f, 0.0f, // top right
-    }; 
-    Renderable paddleB = createRenderable(paddleVerticiesB, 6 * 3);
+    Camera camera = createCamera();
+    GameObject paddleA, paddleB, ball;
+
+    createGameObject("paddle-a", "assets/paddle.v", paddleA);
+    createGameObject("paddle-b", "assets/paddle.v", paddleB);
+    createGameObject("ball", "assets/ball.v", ball);
+
+    unsigned int modelUniformLocation = glGetUniformLocation(shaderProgram, "model");
+    unsigned int viewUniformLocation = glGetUniformLocation(shaderProgram, "view");
+    unsigned int projectionUniformLocation = glGetUniformLocation(shaderProgram, "projection");
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(400.0f, 300.0f, 0.0f));
+
+    glUniformMatrix4fv(viewUniformLocation, 1, GL_FALSE, glm::value_ptr(camera.view));
+    glUniformMatrix4fv(projectionUniformLocation, 1, GL_FALSE, glm::value_ptr(camera.projection));
+
+    paddleA.position[1] += 10.0f;
+
+    paddleB.position[1] += 585.0f;
+
+    ball.position[1] = 300.0f;
+    ball.position[0] = 400.0f;
     // Application Loop
     while(!glfwWindowShouldClose(window_p)) {
         // Process user input
-        processInput(window_p);
+        // processInput(window_p);
+        if (glfwGetKey(window_p, GLFW_KEY_A) == GLFW_PRESS) {
+            paddleB.position[0] -= 2.0f;
+        }
+        if (glfwGetKey(window_p, GLFW_KEY_D) == GLFW_PRESS) {
+            paddleB.position[0] += 2.0f;
+        }
+
+        if (glfwGetKey(window_p, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            paddleA.position[0] -= 2.0f;
+        }
+        if (glfwGetKey(window_p, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+            paddleA.position[0] += 2.0f;
+        }
         
         // Clear screen
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-
-        renderRenderable(paddle);
-        renderRenderable(paddleB);
+        renderGameObject(paddleA, modelUniformLocation);
+        renderGameObject(paddleB, modelUniformLocation);
+        renderGameObject(ball, modelUniformLocation);
 
         // // Swap color buffers
         glfwSwapBuffers(window_p);
@@ -282,7 +386,7 @@ int main() {
         glfwPollEvents();
     }
 
-    deleteRenderable(paddle);
+    // deleteRenderable(paddle);
     // deleteRenderable(paddleB);
     glDeleteProgram(shaderProgram);
 
